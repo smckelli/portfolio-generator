6f528cb7eb2444bbb662269527818c882c0a493a
smckelli <smckelli@gmail.com>
2022-04-18 17:49:11 -0500

    modular-ized the js

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i+=1) {
-    console.log(profileDataArr[i]);
-    }
-    console.log('===============');
+const [name, github] = profileDataArgs;
 
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
