2094c10eccadda9a1d143aba094ca582afd9a565
smckelli <smckelli@gmail.com>
2022-04-18 20:21:57 -0500

    added inquirer prompts

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+const inquirer = require('inquirer');
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
